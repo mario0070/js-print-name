e7642626e2b1d46b4b58b8115beb1163f5d3cda4
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2022-06-21 11:50:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+  // var myname="muhammadjamiu abdulganiyu"
+   // country="nigeria"
+   // height="162 cm"
+    //document.write("<h1>my name : " +myname + "<br>" + "<br>" + "height : " + height + "<br>" + "<br>" + "country : " + country)
+  //  console.log(myname);
+  //  console.log(height);
+   // console.log(country);
+
+
+  //  function hello(name,greet){
+  //   console.log(greet + " my name is " + name)
+  //    return greet.length;
+  //  }
+
+  //  hello("jamiu","hello");
+
+ var color="red"
+  number="123"
+  function hello(){
+    var color="green";
+    var number="12"
+    console.log("scope color is " + color)
+    console.log("scope num is " + number)
+  }
+  hello()
+  console.log("global color is " + color)
+  console.log("global num is " + number)
