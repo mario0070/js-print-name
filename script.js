b7e70bd51d886759638f4d40fcdbcc7eb56bd09c
muhammadjamiu abdulganiyu <105175567+mario0070@users.noreply.github.com>
2022-06-21 11:56:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 
 
 
-  // var myname="muhammadjamiu abdulganiyu"
-   // country="nigeria"
-   // height="162 cm"
-    //document.write("<h1>my name : " +myname + "<br>" + "<br>" + "height : " + height + "<br>" + "<br>" + "country : " + country)
-  //  console.log(myname);
-  //  console.log(height);
-   // console.log(country);
+   var myname="muhammadjamiu abdulganiyu";
+   var country="nigeria";
+   var height="162 cm";
+
+    console.log(myname);
+    console.log(height);
+    console.log(country);
 
 
-  //  function hello(name,greet){
-  //   console.log(greet + " my name is " + name)
-  //    return greet.length;
-  //  }
+   
 
-  //  hello("jamiu","hello");
-
- var color="red"
-  number="123"
-  function hello(){
-    var color="green";
-    var number="12"
-    console.log("scope color is " + color)
-    console.log("scope num is " + number)
-  }
-  hello()
-  console.log("global color is " + color)
-  console.log("global num is " + number)
