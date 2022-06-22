3b6ebdece8d1fc09c145937207328a090ad190e1
muhammadjamiu abdulganiyu <105175567+mario0070@users.noreply.github.com>
2022-06-22 07:34:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
    var country="nigeria";
    var height="162 cm";
 
+    document.write("<h1>my name : " +myname + "<br>" + "<br>" + "height : " + height + "<br>" + "<br>" + "country : " + country);
+
     console.log(myname);
     console.log(height);
     console.log(country);
