e4a33dc7d108ab1da2b55333a7825b7b8c8ba974
muhammadjamiu abdulganiyu <105175567+mario0070@users.noreply.github.com>
2022-06-25 12:15:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  let height = "5 feets"
  let country = "nigeria";
 
-alert("name : "+ name  + "  height : " + height + "  country :  " + country);
+alert("my name : "+ name + "\n" + "height : " + height + "\n" + "country :  " + country);
 document.write("<h1>my name : " + name + "<br>" + "<br>" + "height : " + height + "<br>" + "<br>" + "country : " + country);
 
 
